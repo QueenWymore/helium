add065c79090f8eb7d4c9bfe9c0f453cec30461d
Paweł <p.suminski@gmail.com>
2016-12-08 21:07:15 +0100

    Update routes.php

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -2,20 +2,18 @@
 
 $app->get('/test/:id/:id2', 'AppController:test');
 
-$app->get('/dupa/:id/:name', function($id, $name){
+$app->get('/test2/:id/:name', function($id, $name){
     echo "hello, $name: " . $id;
 });
 
 $app->post('/test/:id', 'AppController:post');
 
-$app->get('/rzal', function(){
-    echo 'rzal xD';
+$app->get('/xd', function(){
+    echo 'xD';
 });
 
-$app->get('/hehe', 'BooksController:index', 'test');
+$app->get('/booksindex', 'BooksController:index', 'test');
 
 $app->resource('books');
 
 $app->resource('posts');
-
-$app->resource('dupa', 'PostsController');
